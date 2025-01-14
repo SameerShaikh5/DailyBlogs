8f1da5301006e91748cbf6ed783846043dfcf437
SameerShaikh5 <166177683+SameerShaikh5@users.noreply.github.com>
2025-01-14 19:51:50 +0530

    Update mongooseConnection.js

--- config/mongooseConnection.js
+++ config/mongooseConnection.js
@@ -5,7 +5,7 @@ const connectDB = async function (){
         await mongoose.connect(`${process.env.MONGODB_URI}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 15000, // 15 seconds
+            serverSelectionTimeoutMS: 15000,
         })
         console.log('Mongodb connected!!')
     } 
